e94634455ca7386c750e71096a421f01a345a6ae
Mike Sanford <michael.sanford@aboutobjects.com>
2024-10-09 08:03:09 -0700

    Initial Commit

---
+++ DomainFun/DomainFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  DomainFunApp.swift
+//  DomainFun
+//
+//  Created by Mike AO on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct DomainFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
